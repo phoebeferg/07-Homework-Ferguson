18aa1df9075b267f65213edd3819169fcdeb16b5
Phoebe <fergusonphoebe@gmail.com>
2025-02-08 15:53:38 -0500

    renderLicenseSection function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -25,8 +25,14 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return `## License
 
+This project is licensed under the ${license} license.`;
+  }
+  return '';
+}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
